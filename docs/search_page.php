664afed147a309eb55241589612784db6ebff433
hajer <s145580@student.squ.edu.om>
2025-12-19 21:37:07 +0400

    Update search_page.php

--- docs/search_page.php
+++ docs/search_page.php
@@ -2,11 +2,11 @@
 
 $host = 'localhost';
 $user = 'root';
-$pass = 'root';
-$dbname = 'gameden_db';
-$port = 8889;
+$pass = '';
+$dbname = 'gamedendb';
 
-$conn = new mysqli($host, $user, $pass, $dbname, $port);
+
+$conn = new mysqli($host, $user, $pass, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
